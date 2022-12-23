458c8117b4754d08a293f529f3eb44002b6eb5b0
Helmi Pradita <helmipraditaa@gmail.com>
2022-12-23 08:54:30 +0700

    update query.sql

--- query.sql
+++ query.sql
@@ -31,3 +31,44 @@ CREATE TABLE tickets (
 );
 
 DROP TABLE tickets;
+
+CREATE TABLE tbl_bookings (
+    id VARCHAR PRIMARY KEY,
+    users_id VARCHAR,
+	tickets_id VARCHAR,
+	passenger_title VARCHAR,
+	passenger_name VARCHAR,
+    passenger_country VARCHAR,
+    payment INTEGER,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+DROP TABLE tbl_bookings;
+
+UPDATE tickets SET stock = stock - 1 FROM tbl_bookings
+  WHERE tickets.id = '39807d02-d484-48ad-9f10-feb074bc077d';
+
+
+CREATE TABLE tbl_book (
+    id VARCHAR PRIMARY KEY,
+    users_id VARCHAR,
+	ticket_id VARCHAR
+);
+
+CREATE TABLE tbl_ticket (
+    id VARCHAR PRIMARY KEY,
+    stock INT
+);
+
+DROP TABLE tbl_book;
+
+CREATE OR REPLACE FUNCTION auditstock() RETURNS TRIGGER AS $example_table$
+BEGIN
+UPDATE tickets SET stock = stock - 1, updated_at = NOW() FROM booking WHERE tickets.id = booking.id_tickets;
+RETURN NEW;
+END;
+$example_table$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_stock
+AFTER INSERT ON booking FOR EACH ROW EXECUTE PROCEDURE auditstock();
