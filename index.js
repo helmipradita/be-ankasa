ec0e8388693e4c3739162b65799f67482c2d8b9a
Rizky S <68472767+rizkysyp@users.noreply.github.com>
2023-01-01 19:58:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://ankasa.rizkyproject.my.id",
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
